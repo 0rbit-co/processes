215ab2b7d5c84460dd28e6246238e0d0cdc3c454
lucifer0x17 <ayush.aa347@gmail.com>
2024-04-27 22:26:42 +0530

    feat: v1 completed

--- oracle.lua
+++ oracle.lua
@@ -4,7 +4,7 @@ local json = require('json')
 local bint = require('.bint')(256)
 
 _MIN_FEE = bint(2 * 1e11)
-_MAX_FEE = bint(2 * 1e12)
+_MAX_FEE = bint(1e12)
 _FREE_CREDITS = 50
 _TOKEN_PROCESS = "BUhZLMwQ6yZHguLtJYA5lLUa9LQzLXMXRfaq9FVcPJc"
 
@@ -33,7 +33,14 @@ local utils = {
     end,
     lt = function(a, b)
         return bint.__lt(a, b)
+    end,
+    mergeTables = function(t1, t2)
+        for key, value in pairs(t2) do
+            t1[key] = value -- Overwrites existing key in t1 if it exists
+        end
+        return t1
     end
+
 }
 
 function isGetRequest(Msg)
@@ -76,10 +83,27 @@ Handlers.add('getData',
         else
             maxFee = _MAX_FEE
         end
-        local validFor = tags.ValidFor or 0
+        local validFor = tags["X-ValidFor"] or 0
 
         local sender = tags.Sender
         local url = tags["X-Url"]
+
+        if IS_ERROR == 1 then
+            Send({
+                Target = sender,
+                Data = Colors.red ..
+                    "MAINTAINANCE: Some issue going on with process" ..
+                    Colors.green ..
+                    " Contact 0RBIT team"
+            })
+            Send({
+                Target = _TOKEN_PROCESS,
+                Action = "Transfer",
+                Recipient = sender,
+                Quantity = utils.toBalanceValue(recievedFee)
+            })
+            return
+        end
 
         -- -- check url
         -- if not isValidUrl(url) then
@@ -101,7 +125,10 @@ Handlers.add('getData',
             Send({
                 Target = sender,
                 Data = Colors.red ..
-                    "ERROR: Transferred less fee than maxFee"
+                    "ERROR: Transferred less fee than maxFee" ..
+                    Colors.green ..
+                    " Recieved:" ..
+                    utils.toBalanceValue(recievedFee) .. Colors.red .. " Expected:" .. utils.toBalanceValue(maxFee)
             })
             Send({
                 Target = _TOKEN_PROCESS,
@@ -121,7 +148,74 @@ Handlers.add('getData',
             Recipient = sender,
             Target = ao.id
         }
-        Handlers.utils.reply("Message recieved for url:" .. url)(Msg)
+        Send({
+            Target = sender,
+            Data = "Message recieved for url:" .. url
+        })
+
+        table.insert(LOGS, {
+            MessageId = Msg.Id,
+            Recipient = sender,
+        })
+    end
+)
+
+Handlers.add('getSponsoredData',
+    Handlers.utils.hasMatchingTag("Action", "Get-Real-Data"),
+    function(Msg)
+        local msgId = Msg.Id
+        local tags = Msg.Tags
+        -- local minFee = bint(tags["X-Min-Fee"]) or _MIN_FEE
+        local minFee = _MIN_FEE
+        -- local maxFee = bint(tags["X-Max-Fee"]) or _MAX_FEE
+        local maxFee = _MAX_FEE
+
+        -- -- TODO: check timeperiod /timestamp
+        local validFor = tags["ValidFor"] or 0
+
+        local sender = Msg.From
+        local url = tags["Url"]
+
+        if IS_ERROR == 1 then
+            Send({
+                Target = sender,
+                Data = Colors.red ..
+                    "MAINTAINANCE: Some issue going on with process" ..
+                    Colors.green ..
+                    " Contact 0RBIT team"
+            })
+            return
+        end
+
+        -- -- TODO: add handler to Check Free requests
+        if sender then
+            if FREE_QUOTA[sender] and FREE_QUOTA[sender] <= 0 then
+                Handlers.utils.reply("Free Quota has been completed. Contact the team for more requests")(Msg)
+                return
+            elseif not FREE_QUOTA[sender] then
+                FREE_QUOTA[sender] = 50
+            end
+
+            FREE_QUOTA[sender] = FREE_QUOTA[sender] - 1
+        else
+            print("Error: Sender is nil.")
+            Handlers.utils.reply("No sender found")(Msg)
+            return
+        end
+
+        GET_REQUESTS[msgId] = {
+            Url = url,
+            MinFee = utils.toBalanceValue(minFee),
+            MaxFee = utils.toBalanceValue(maxFee),
+            ValidFor = validFor,
+            Timestamp = Msg.Timestamp,
+            Recipient = sender,
+            Target = ao.id
+        }
+        Send({
+            Target = sender,
+            Data = "Message recieved for url:" .. url
+        })
 
         table.insert(LOGS, {
             MessageId = Msg.Id,
@@ -147,7 +241,7 @@ Handlers.add('recieveData',
         local tags = Msg.Tags
         local requestId = tags["Request-Msg-Id"]
         -- First we will check processed request to make sure someone hasn't answeered already
-        if not PROCESSED_REQUESTS[requestId] then
+        if PROCESSED_REQUESTS[requestId] then
             Handlers.utils.reply("ID:" .. requestId .. "Request has already been processed")(Msg)
             table.insert(LOGS, {
                 MessageId = Msg.Id,
@@ -155,7 +249,7 @@ Handlers.add('recieveData',
             })
             return
         end
-        if not GET_REQUESTS[requestId] or not POST_REQUESTS[requestId] then
+        if not (GET_REQUESTS[requestId] or POST_REQUESTS[requestId]) then
             Handlers.utils.reply("No request found with the Id of" .. requestId)(Msg)
             table.insert(LOGS, {
                 MessageId = Msg.Id,
@@ -171,7 +265,7 @@ Handlers.add('recieveData',
             request = POST_REQUESTS[requestId]
         end
 
-        if tags.Fee > request.MaxFee then
+        if utils.toNumber(tags.Fee) > utils.toNumber(request.MaxFee) then
             Handlers.utils.reply("ID:" .. requestId .. "Fees asked is greater than the suggested range by the user")(Msg)
             table.insert(LOGS, {
                 MessageId = Msg.Id,
@@ -179,31 +273,40 @@ Handlers.add('recieveData',
             })
             return
         end
-        if Msg.Timestamp > (request.Timestamp + request.ValidFor) then
-            FAILED_REQUESTS[requestId] = GET_REQUESTS[requestId]
-            GET_REQUESTS[requestId] = nil
-            Handlers.utils.reply("ID:" .. requestId .. "Request has been timedout")(Msg)
-            table.insert(LOGS, {
-                MessageId = Msg.Id,
-                Status = "TIMEDOUT"
-            })
-            return
+        if request.ValidFor ~= 0 then
+            local expirationTime = request.Timestamp + request.ValidFor
+            if Msg.Timestamp > expirationTime then
+                FAILED_REQUESTS[requestId] = GET_REQUESTS[requestId]
+                GET_REQUESTS[requestId] = nil
+                -- TODO: MAKE FUBNCTIONALITY IN NODE TO READ THIS MESSAGE
+                Handlers.utils.reply("ID:" .. requestId .. "Request has been timedout")(Msg)
+                table.insert(LOGS, {
+                    MessageId = Msg.Id,
+                    Status = "TIMEDOUT"
+                })
+                return
+            end
         end
         -- Verify data logic will come here
         -- Rewarding node logic will come here and for FREE_QUOTA reward will be sent from
-        -- Transfer back the remaining amount
-        Send({
+        local responseMessage = {
             Target = request.Recipient,
             Data = Msg.Data,
             FeeUsed = tags.Fee
             -- add other tags that is needed also add headers
-        })
+        }
+        -- utils.mergeTables(responseMessage, Msg.Tags)
+        responseMessage["Action"] = "Receive-Data-Feed"
+        responseMessage["content-type"] = "text/html; charset=utf-8"
+        Send(responseMessage)
+        -- Transfer back the remaining amount
         PROCESSED_REQUESTS[requestId] = {
             ResponseMsg = Msg.Id,
             RequestMsg = request,
             Fee = tags.Fee,
-            TimeTaken = Msg.Timestamp - (request.ValidFor + request.Timestamp)
+            -- TimeTaken = Msg.Timestamp - (request.ValidFor + request.Timestamp)
         }
+        GET_REQUESTS[requestId] = nil
         table.insert(LOGS, {
             MessageId = Msg.Id,
             Status = "SUCCESS"
@@ -270,6 +373,6 @@ Send({
     Recipient =
     "4_jJUtiNjq5Xrg8OMrEDo-_bud7p5vbSJh1e69VJ76U",
     Quantity = "1000000000000",
-    ["X-Url"] = "https://www.google.com",
+    ["X-Url"] = "https://lucifer0x17.g8way.0rbit.co",
     ["X-Action"] = "Get-Real-Data"
 })
